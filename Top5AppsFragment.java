48f1cc8cf7507aeb68cf87878d6442c4217d2819
Tuomo Kinnunen <tuomo95@gmail.com>
2017-06-06 15:46:52 +0300

    Fragment-luokka

---
+++ Top5AppsFragment.java
@@ -0,0 +1,21 @@
+package tuomomees.screentimecalculator;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by tuomo on 6.6.2017.
+ */
+
+public class Top5AppsFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view, container, false);
+    }
+}
